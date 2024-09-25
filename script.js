2e859b10ada0bfd0f64ee9722683be336df66ead
Brittany Anderson <142555405+brittanyanderson1@users.noreply.github.com>
2024-09-24 23:47:21 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+/*
+M2: Assignment 1
+Author: Brittany Anderson
+Date: 9/24/2024
+ */
+
+// STEP 1: Convert the following highlighted identifiers to Camel Case notation:
+
+//let some_month --> let someMonth
+//function the Month() --> function theMonth()
+//let current-month --> let currentMonth
+//let summer_month --> let summerMonth
+//let MyLibrary-function --> let MyLibraryFunction
+
+// STEP 2: Give me an example of a numeric literal expression, a string literal expression, a Boolean literal expression,
+// and a null literal expression.
+
+// Numeric Literal Expression: 42
+// String Literal Expression: "Howdy Yall!"
+// Boolean Literal Expression: True
+// Null Literal Expression: null
+
+// STEP 3
+// STEP 4
+// STEP 5
+// STEP 6
+// STEP 7
+// STEP 8
+// STEP 9
+// STEP 10
+// STEP 11
+// STEP 12
+// STEP 13
+// STEP 14
+// STEP 15
+// STEP 16
+// STEP 17
+// STEP 18
+// STEP 19
+// STEP 20
+// STEP 21
+// STEP 22
